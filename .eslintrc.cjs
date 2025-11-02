cbcfbec5d810d97376314815dd9c6265b24dbdef
Veronica Tankpinu <veronicasenami977@gmail.com>
2025-11-02 23:13:05 +0100

    add ESLint config for Vite + React + TypeScript

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+// ESLint config for Vite + React + TypeScript
+module.exports = {
+  root: true,
+  env: { browser: true, es2022: true, node: true },
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: 'detect' } },
+  plugins: ['react-refresh', 'react-hooks', '@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended'
+  ],
+  rules: {
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }]
+  },
+  ignorePatterns: ['dist', 'node_modules']
+}
